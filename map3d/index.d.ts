5f8e55ad5823c68056f55e1477df94b88224eaf7
wangxingkang <156148958@qq.com>
2020-10-16 12:22:57 +0800

    feat: 补充部分类型

---
+++ map3d/index.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="../base" />
+
+/// <reference path="geometry-3d.d.ts" />
+/// <reference path="lights.d.ts" />
+/// <reference path="map3d.d.ts" />
+/// <reference path="object3d-group.d.ts" />
+/// <reference path="object3d-layer.d.ts" />
+/// <reference path="object3d.d.ts" />
+/// <reference path="vector3.d.ts" />
